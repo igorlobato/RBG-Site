e4ce0defc8828daaa911e132e07c250776ab9f59
Igor Lobato <igor_stm@yahoo.com.br>
2024-09-23 20:06:58 -0300

    Projeto criado e migrations iniciais do banco de dados feitos

---
+++ database/migrations/2024_09_23_192055_curtidaspost.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('curtidasposts', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->unsignedBigInteger('id_post');
+            $table->foreign('id_post')->references('id')->on('posts')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('curtidas');
+    }
+};
